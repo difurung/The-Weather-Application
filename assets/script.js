3587bd173a32ec7c06018b6a531dad7e72517ae8
denart ifurung <dfurung@hotmail.com>
2022-11-19 02:14:00 -0500

    Added HTML. Formatted using bootstrap

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+//  5 day API key : ced6331887cb8168b2eccb551eab7480 
+// http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ced6331887cb8168b2eccb551eab7480
+
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ced6331887cb8168b2eccb551eab7480";
